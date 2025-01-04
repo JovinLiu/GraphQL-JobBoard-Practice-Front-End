615500957a4c20d5d159174518cf43ecc9e9a9c1
Jovin Liu <liujovin@gmail.com>
2025-01-05 02:46:12 +1100

    20250105

---
+++ src/lib/graphql/queries.js
@@ -0,0 +1,143 @@
+import { getAccessToken } from "../auth";
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  concat,
+  createHttpLink,
+  gql,
+} from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:9000/graphql",
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  const token = getAccessToken();
+  if (token) {
+    operation.setContext({
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  }
+  return forward(operation);
+});
+
+export const apolloClinet = new ApolloClient({
+  // uri: "http://localhost:9000/graphql",
+  link: concat(authLink, httpLink),
+  cache: new InMemoryCache(),
+  //设置全局invalidate Data的中心位置
+  // defaultOptions: {
+  //   query: {
+  //     fetchPolicy: "network-only",
+  //   },
+  //   watchQuery: {
+  //     fetchPolicy: "network-only",
+  //   },
+  // },
+});
+
+const jobDetailFragment = gql`
+  fragment JobDetail on Job {
+    id
+    date
+    title
+    company {
+      id
+      name
+    }
+    description
+  }
+`;
+
+export const companyByIdQuery = gql`
+  query getCompanyById($id: ID!) {
+    company(id: $id) {
+      id
+      name
+      description
+      jobs {
+        id
+        date
+        title
+      }
+    }
+  }
+`;
+
+const jobByIdQuery = gql`
+  query getJobById($id: ID!) {
+    job(id: $id) {
+      ...JobDetail
+    }
+  }
+  ${jobDetailFragment}
+`;
+
+export async function createJob({ title, description }) {
+  const mutation = gql`
+    mutation CreateJob($input: CreateJobInput!) {
+      job: createJob(input: $input) {
+        ...JobDetail
+      }
+    }
+    ${jobDetailFragment}
+  `;
+
+  const { data } = await apolloClinet.mutate({
+    mutation,
+    variables: { input: { title, description } },
+    update: (cache, result) => {
+      cache.writeQuery({
+        query: jobByIdQuery,
+        variables: { id: result.data.job.id },
+        data: result.data,
+      });
+    },
+    //可以在这里传入第三个Argument来实现authentication
+    // context: {
+    //   headers: {}
+    // }
+  });
+
+  return data.job;
+}
+
+export async function getJob(id) {
+  const { data } = await apolloClinet.query({
+    query: jobByIdQuery,
+    variables: { id },
+  });
+  return data.job;
+}
+
+// export async function getCompany(id) {
+//   const { data } = await apolloClinet.query({
+//     query: companyByIdQuery,
+//     variables: { id },
+//   });
+//   return data.company;
+// }
+
+export async function getJobs() {
+  const query = gql`
+    query getJobs {
+      jobs {
+        id
+        date
+        title
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+
+  const { data } = await apolloClinet.query({
+    query,
+    // fetchPolicy: "cache-first", 可以在这里设置reinvalidate数据的设置， cache first表示缓存优先数据
+    fetchPolicy: "network-only", //总是获取最新的数据, network first表示数据库数据优先
+  });
+  return data.jobs;
+}
